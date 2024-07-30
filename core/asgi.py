a23b0aeb42600ab2679b29b1c5f2696ff53b68a3
sina <mubarriizz@gmail.com>
2024-07-30 18:40:16 +0330

    [rebase] use channels for web socket protocols

---
+++ core/asgi.py
@@ -0,0 +1,14 @@
+import os
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from editor.routing import websocket_urlpatterns
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
+    }
+)
